72445ddc798d2661ace71d2e0bc1efcdbf1e581f
FrancescaRodricks <francescarodricks5@gmail.com>
2018-05-28 09:21:58 +0100

    refactored case to method and frequent renter points calculation to a method

--- customer.rb
+++ customer.rb
@@ -15,38 +15,37 @@ class Customer
   def statement
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
-      this_amount = 0
-
-      # determine amounts for each line
-      case element.movie.price_code
-      when ::Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when ::Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when ::Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
-
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == ::Movie::NEW_RELEASE && element.days_rented > 1
-
+    @rentals.each do |rental|
+      frequent_renter_points += calculate_frequent_renter_points(frequent_renter_points, rental)
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + rental.movie.title + "\t" + amount_for(rental).to_s + "\n"
+      total_amount += amount_for(rental)
     end
     # add footer lines
     result += "Amount owed is #{total_amount.to_s}\n"
     result += "You earned #{frequent_renter_points.to_s} frequent renter points"
+    puts "ORIGINAL AND REFACTORED RESULTS ARE THE SAME ? #{result == "Rental Record for Francesca\n\tShrek\t4.5\nAmount owed is 4.5\nYou earned 1 frequent renter points"}"
     result
   end
+
+  def calculate_frequent_renter_points(frequent_renter_points, rental)
+    (rental.movie.price_code == ::Movie::NEW_RELEASE && rental.days_rented > 1) ? 2 : 1
+  end
+
+  def amount_for(rental)
+    this_amount = 0
+    case rental.movie.price_code
+    when ::Movie::REGULAR
+      this_amount += 2
+      this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+    when ::Movie::NEW_RELEASE
+      this_amount += rental.days_rented * 3
+    when ::Movie::CHILDRENS
+      this_amount += 1.5
+      this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+    end
+  end
 end
-
-
 
 movie = Movie.new(title: 'Shrek', price_code: 1)
 rental = Rental.new(movie: movie, days_rented: 5)
