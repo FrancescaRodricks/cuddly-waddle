f32198f3662d2f68b0540c2d66b192b2cb99deda
FrancescaRodricks <francescarodricks5@gmail.com>
2018-05-28 14:38:58 +0100

    refactored total amount & frequent renter points

--- customer.rb
+++ customer.rb
@@ -13,13 +13,10 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
-      frequent_renter_points += rental.calculate_frequent_renter_points
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
-      total_amount += rental.charge
     end
     # add footer lines
     result += "Amount owed is #{total_amount.to_s}\n"
@@ -27,6 +24,27 @@ class Customer
     puts "ORIGINAL AND REFACTORED RESULTS ARE THE SAME ? #{result == "Rental Record for Francesca\n\tShrek\t4.5\nAmount owed is 4.5\nYou earned 1 frequent renter points"}"
     result
   end
+
+  def frequent_renter_points
+    # frequent_renter_points = 0
+    # @rental.each do |rental|
+    #   frequent_renter_points += rental.calculate_frequent_renter_points
+    # end
+    # frequent_renter_points
+    # REFACTORED TO
+    @rentals.inject(0.0) { |total_points, rental| total_points += rental.calculate_frequent_renter_points }
+  end
+
+  def total_amount
+    # total_amount = 0
+    # @rental.each do |rental|
+    #   total_amount += rental.charge
+    # end
+    # total_amount
+    # REFACTORED TO
+    @rentals.inject(0.0) { | total_amount, rental| total_amount += rental.charge }
+  end
+
 
 end
 
@@ -36,7 +54,7 @@ rental = Rental.new(movie: movie, days_rented: 5)
 rental2 = Rental.new(movie: movie2, days_rented: 10)
 customer = Customer.new(name:'Francesca')
 customer.rentals << rental
-customer.rentals << rental2
+# customer.rentals << rental2
 p customer.statement
 
 
