763488c80b30596f7767443ebc08632976658536
FrancescaRodricks <francescarodricks5@gmail.com>
2018-06-02 19:33:58 +0100

    method missing for class methods

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -13,7 +13,20 @@ class Book
       end
     end
 
+    def method_missing(method_name, *arguments, &block)
+      puts "#{method_name} with #{arguments} #{block.call if block_given?} not defined"
+      # super
+    end
   end
+
 end
 
 Book.title_details("Famous Five")
+
+
+# method missing
+
+Book.borrow
+
+
+Book.sell("New Customer") { "John Doe" }
