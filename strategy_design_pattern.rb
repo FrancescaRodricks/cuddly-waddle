b24788557bc21c02ab504b2489f28c7a69dda998
FrancescaRodricks <francescarodricks5@gmail.com>
2018-11-11 22:58:44 +0000

    pass data from context to strategy by passing a reference of the context to the strategy

--- strategy_design_pattern.rb
+++ strategy_design_pattern.rb
@@ -33,6 +33,7 @@ end
 
 
 class Report
+  attr_accessor :formatter
   def initialize(formatter)
     @title = "Monthly Report"
     @text = ["Things are going", "really really well"]
@@ -44,5 +45,75 @@ class Report
   end
 end
 
-Report.new(HTMLFormatter.new).output_report
-Report.new(PlainTextFormatter.new).output_report
+report = Report.new(HTMLFormatter.new)
+report.output_report
+
+# select a strategy class at runtime
+report.formatter = PlainTextFormatter.new
+
+report.output_report
+# Report.new(PlainTextFormatter.new).output_report
+
+
+
+# Pass data from the context to the strategy
+# Context - the Report class pass self
+# Strategy - Formatter, HTMLFormatter, PlainTextFormatter
+
+
+class Formatter
+  def output_report(context)
+    raise "Called #{__method__} from abstract class"
+  end
+end
+
+class HTMLFormatter < Formatter
+  def output_report(context)
+    puts('<html>')
+    puts('<head>')
+    puts("<title>#{context.title}</title>")
+    puts('</head>')
+    puts('<body>')
+    context.text.each do |text|
+      puts("<p>#{text}</p>")
+    end
+    puts('</body>')
+    puts('</html>')
+  end
+end
+
+
+class PlainTextFormatter < Formatter
+  def output_report(context)
+    puts "-- #{ context.title } --"
+    context.text.each do |t|
+      puts("--- #{t} ---")
+    end
+  end
+end
+
+
+class Report
+
+  attr_accessor :formatter, :title, :text
+
+  def initialize(formatter)
+    @title = "Monthly Report"
+    @text = ["Things are going", "really really well"]
+    @formatter = formatter
+  end
+
+  def output_report
+    @formatter.output_report(self)
+  end
+end
+
+report = Report.new(HTMLFormatter.new)
+report.output_report
+
+# select a strategy class at runtime
+report.formatter = PlainTextFormatter.new
+
+report.output_report
+# Report.new(PlainTextFormatter.new).output_report
+
