e68138aee770648dfee615d00dd707655962cd6b
FrancescaRodricks <francescarodricks5@gmail.com>
2018-11-14 23:57:23 +0000

    composite design pattern - initial

---
+++ composite_design_pattern.rb
@@ -0,0 +1,78 @@
+# Composite design pattern
+# Component - the base class for all the objects - example here - Task Class
+# Manufacture Cake
+  # Make Cake -> Make Batter -> AddDryIngredients, AddLiquids, Mix
+  #           -> Fill Pan
+  #           -> Bake
+  #           -> Frost
+  # Package Cake -> Box, Label
+
+class Task
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_time_required
+    0.0
+  end
+end
+
+class AddDryIngredientsTask < Task
+  def initialize
+    super('Add dry ingredients')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+class AddLiquidsTask < Task
+  def initialize
+    super('Add liquids')
+  end
+
+  def get_time_required
+    2.0
+  end
+
+end
+
+class MixTask < Task
+  def initialize
+    super('Mix')
+  end
+
+  def get_time_required
+    3.0
+  end
+end
+
+
+class MakeBatterTask < Task
+  attr_accessor :subtasks
+
+  def initialize
+    super('make batter')
+    @subtasks = []
+    add_subtasks(AddDryIngredientsTask.new)
+    add_subtasks(AddLiquidsTask.new)
+    add_subtasks(MixTask.new)
+  end
+
+  def add_subtasks(task)
+    subtasks.push(task)
+  end
+
+  def remove_subtask(task)
+    subtasks.delete(task)
+  end
+
+  def get_time_required
+    time = 5.0
+    time += subtasks.map { |subtask| subtask.get_time_required }.reduce(:+)
+  end
+end
+
+p MakeBatterTask.new.get_time_required
